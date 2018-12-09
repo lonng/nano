234914bd196dd8c44fffdf61534eaad5b4e42541
Lonng <chris@lonng.org>
2018-12-09 16:01:45 +0800

    timer: fix unit tests

--- timer_test.go
+++ timer_test.go
@@ -7,6 +7,16 @@ import (
 )
 
 func TestNewTimer(t *testing.T) {
+	var exists = struct {
+		timers        int
+		createdTimes  int
+		closingTimers int
+	}{
+		timers:        len(timerManager.timers),
+		createdTimes:  len(timerManager.createdTimer),
+		closingTimers: len(timerManager.closingTimer),
+	}
+
 	const tc = 1000
 	var counter int64
 	for i := 0; i < tc; i++ {
@@ -22,20 +32,30 @@ func TestNewTimer(t *testing.T) {
 		t.Fatalf("expect: %d, got: %d", tc*2, counter)
 	}
 
-	if len(timerManager.timers) != tc {
+	if len(timerManager.timers) != exists.timers+tc {
 		t.Fatalf("timers: %d", len(timerManager.timers))
 	}
 
-	if len(timerManager.createdTimer) != 0 {
+	if len(timerManager.createdTimer) != exists.createdTimes {
 		t.Fatalf("createdTimer: %d", len(timerManager.createdTimer))
 	}
 
-	if len(timerManager.closingTimer) != 0 {
+	if len(timerManager.closingTimer) != exists.closingTimers {
 		t.Fatalf("closingTimer: %d", len(timerManager.closingTimer))
 	}
 }
 
 func TestNewAfterTimer(t *testing.T) {
+	var exists = struct {
+		timers        int
+		createdTimes  int
+		closingTimers int
+	}{
+		timers:        len(timerManager.timers),
+		createdTimes:  len(timerManager.createdTimer),
+		closingTimers: len(timerManager.closingTimer),
+	}
+
 	const tc = 1000
 	var counter int64
 	for i := 0; i < tc; i++ {
@@ -50,15 +70,15 @@ func TestNewAfterTimer(t *testing.T) {
 		t.Fatalf("expect: %d, got: %d", tc, counter)
 	}
 
-	if len(timerManager.timers) != 0 {
+	if len(timerManager.timers) != exists.timers {
 		t.Fatalf("timers: %d", len(timerManager.timers))
 	}
 
-	if len(timerManager.createdTimer) != 0 {
+	if len(timerManager.createdTimer) != exists.createdTimes {
 		t.Fatalf("createdTimer: %d", len(timerManager.createdTimer))
 	}
 
-	if len(timerManager.closingTimer) != 0 {
+	if len(timerManager.closingTimer) != exists.closingTimers {
 		t.Fatalf("closingTimer: %d", len(timerManager.closingTimer))
 	}
 }
