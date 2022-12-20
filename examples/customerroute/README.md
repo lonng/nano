3b502eac9db3e754e5f4369ddfa7cbeaff90fb27
zxin <282527991@qq.com>
2022-12-20 18:56:00 +0800

    detail

--- examples/customerroute/README.md
+++ examples/customerroute/README.md
@@ -17,3 +17,11 @@ go build
 ./customerroute gate --listen "127.0.0.1:34570" --gate-address "127.0.0.1:34590"
 ```
 
+## open browser and visit url for 4 times
+```
+http://127.0.0.1:12345/web/ 
+http://127.0.0.1:12345/web/ 
+http://127.0.0.1:12345/web/ 
+http://127.0.0.1:12345/web/     
+```
+input content and send, the same ChatRoomService node will sync the message each other
--- examples/customerroute/tworoom/chat_service.go
+++ examples/customerroute/tworoom/chat_service.go
@@ -1,14 +1,12 @@
 package tworoom
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/lonng/nano"
 	"github.com/lonng/nano/component"
 	"github.com/lonng/nano/examples/cluster/protocol"
 	"github.com/lonng/nano/session"
-	"github.com/pingcap/errors"
 )
 
 type ChatRoomService struct {
@@ -23,12 +21,6 @@ func newChatRoomService() *ChatRoomService {
 }
 
 func (rs *ChatRoomService) JoinRoom(s *session.Session, msg *protocol.JoinRoomRequest) error {
-	broadcast := &protocol.NewUserBroadcast{
-		Content: fmt.Sprintf("User user join: %v", msg.Nickname),
-	}
-	if err := rs.group.Broadcast("onNewUser", broadcast); err != nil {
-		return errors.Trace(err)
-	}
 	return rs.group.Add(s)
 }
 
