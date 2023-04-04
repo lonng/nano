7df98dac726ac85aaf1ae4bf3ff69e6741d2aa7e
van <no-replay@xgmail.com>
2023-04-04 09:34:04 +0800

    route delete bind

--- session/router.go
+++ session/router.go
@@ -36,6 +36,13 @@ func (r *Router) Bind(service, address string) {
 	r.routes.Store(service, address)
 }
 
+// Delete route delete bind for
+// 1. some son-service restart, sadly it's not auto bind new son-service
+// 2. some game have match to some service, delete bind and match customer service
+func (r *Router) Delete(service string) {
+	r.routes.Delete(service)
+}
+
 // Find finds the address corresponding a remote service
 func (r *Router) Find(service string) (string, bool) {
 	v, found := r.routes.Load(service)
